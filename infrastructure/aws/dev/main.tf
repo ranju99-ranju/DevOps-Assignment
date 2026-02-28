f2d0d6e2f0b11b173761261377bfa4566abb0e98
ranju99-ranju <ranjthaml@gmail.com>
2026-02-28 05:24:16 -0800

    Add AWS provider and dev instance configuration

---
+++ infrastructure/aws/dev/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev_instance" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "dev-instance"
+    Environment = "dev"
+  }
+}
